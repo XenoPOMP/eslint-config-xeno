01783710bf8e969cb65461b19f04cd25d1fcdd5c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-04 14:00:56 +0300

    📝 update config

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-module.exports = {
-  'extends': [],
+// @ts-check
+
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    '@ririd',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
